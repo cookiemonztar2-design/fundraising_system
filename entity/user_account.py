e585a681a1516de56c9e06316ccdbf544f8cdb10
cookiemonztar2-design <cookiemonztar2@gmail.com>
2026-05-01 18:25:15 +0800

    Initial commit - Flask user management system

---
+++ entity/user_account.py
@@ -0,0 +1,10 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class UserAccount(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    role= db.Column(db.String(50), nullable=False)
+    status = db.Column(db.string(20), default="Active")
