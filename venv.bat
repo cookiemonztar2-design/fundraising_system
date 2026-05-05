665a5f8b6998065eef2d419684b1c2d4482abc7b
xiazixin <xia.zixin@yahoo.com>
2026-05-05 10:46:06 +0800

    Fix virtual environment setup path in venv.bat

--- venv.bat
+++ venv.bat
@@ -1,7 +1,7 @@
 RME setup python venv
 @echo off
-python3 -m venv %userprofile%\fundraising_system\venv
-%userprofile%\fundraising_system\venv\Scripts\activate.bat
+python3 -m venv ..\fundraising_system\venv
+..\fundraising_system\venv\Scripts\activate.bat
 pip install -r requirements.txt
 echo Virtual environment setup complete. To activate it, run:
-echo %userprofile%\fundraising_system\venv\Scripts\activate.bat
+echo ..\fundraising_system\venv\Scripts\activate.bat
